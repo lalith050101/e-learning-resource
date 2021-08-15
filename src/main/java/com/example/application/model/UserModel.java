99af82f791817e01ba0ab002dc8cde3165caac4b
YADLAPALLI VENKATA LALITH KUMAR <54011175+lalith050101@users.noreply.github.com>
2021-08-15 21:01:16 +0530

    Update UserModel.java

--- src/main/java/com/example/application/model/UserModel.java
+++ src/main/java/com/example/application/model/UserModel.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
 
 @Entity
 public class UserModel {
