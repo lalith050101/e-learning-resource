d0e184c07aa9cd4b8190ace6c4fffdd5d1487f79
lalith050101 <yvlalithkumar@gmail.com>
2021-08-15 09:30:35 +0530

    updated user controller

--- src/main/java/com/example/application/controller/UserController.java
+++ src/main/java/com/example/application/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.example.application.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import java.util.List;
-import com.example.application.model.LoginModel;
-import com.example.application.model.UserModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.application.repository.UserModelRepository;
 
 @CrossOrigin(origins = {"*"})
@@ -20,13 +20,13 @@ public class UserController {
 	private UserModelRepository userModelRepository;
 	
 	@GetMapping("/admin")
-	public ResponseEntity<List<?>> getAllUsers() {
+	public ResponseEntity<?> getAllUsers() {
         try{
 			return new ResponseEntity<List<?>> (userModelRepository.findAll(), HttpStatus.OK);
 		
 		}
 		catch(Exception e) {
-			return new ResponseEntity<String> ("server error", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String> ("error", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
--- src/main/java/com/example/application/model/ResourceModel.java
+++ src/main/java/com/example/application/model/ResourceModel.java
@@ -2,6 +2,7 @@ package com.example.application.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
