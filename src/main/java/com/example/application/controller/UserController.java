cd7b4158d8d20fee483af3a483efd1c6926571de
lalith050101 <yvlalithkumar@gmail.com>
2021-08-15 09:06:10 +0530

    added user controller

---
+++ src/main/java/com/example/application/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.application.model.LoginModel;
+import com.example.application.model.UserModel;
+import com.example.application.repository.UserModelRepository;
+
+@CrossOrigin(origins = {"*"})
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserModelRepository userModelRepository;
+	
+	@GetMapping("/admin")
+	public ResponseEntity<List<?>> getAllUsers() {
+        try{
+			return new ResponseEntity<List<?>> (userModelRepository.findAll(), HttpStatus.OK);
+		
+		}
+		catch(Exception e) {
+			return new ResponseEntity<String> ("server error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
